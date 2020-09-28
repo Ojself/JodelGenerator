e3a3bd465cb1c3dbf619a458a0691ec615344451
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-09-28 21:00:34 +0200

    Updates

---
+++ precache-manifest.0ce166fc06484e8a7df70ea3616cff2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab32747d331809e9d3f0a5761492b3bf",
+    "url": "/JodelGenerator/index.html"
+  },
+  {
+    "revision": "4b90abd7b17761e97235",
+    "url": "/JodelGenerator/static/css/main.36290368.chunk.css"
+  },
+  {
+    "revision": "9492249416d6d65a3b73",
+    "url": "/JodelGenerator/static/js/2.4274fc94.chunk.js"
+  },
+  {
+    "revision": "afb3eeb32446bd58091029392c041073",
+    "url": "/JodelGenerator/static/js/2.4274fc94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b90abd7b17761e97235",
+    "url": "/JodelGenerator/static/js/main.73ea531b.chunk.js"
+  },
+  {
+    "revision": "af819f1196eb1097a24b",
+    "url": "/JodelGenerator/static/js/runtime-main.81ffc0af.js"
+  }
+]);
